817d1c96977ea6f802c42e49900573d302ff7dbc
Reza Fabriza <58038780+RezaFab@users.noreply.github.com>
2020-10-20 14:12:29 +0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,7 @@
-/*!
-    * Start Bootstrap - Resume v6.0.1 (https://startbootstrap.com/template-overviews/resume)
-    * Copyright 2013-2020 Start Bootstrap
-    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
-    */
+
     (function ($) {
     "use strict"; // Start of use strict
 
-    // Smooth scrolling using jQuery easing
     $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
         if (
             location.pathname.replace(/^\//, "") ==
