d38512514cb840a9499547c6459000c1e6c712c1
Reza Fabriza <58038780+RezaFab@users.noreply.github.com>
2020-10-20 13:40:49 +0700

    Add files via upload

---
+++ js/2.js
@@ -0,0 +1,3 @@
+function displayResult1() {
+  var x = document.getElementsByClassName("example");x[0].innerHTML = "Have a nice day Guys!";
+}
---
+++ js/3.js
@@ -0,0 +1,4 @@
+function displayResult2() {
+  var x = document.getElementsByTagName("a");
+  x[0].innerHTML = "Have a nice day All!";
+}
